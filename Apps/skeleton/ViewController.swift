e06f65f9b3ee8d430cf33b4e29ad2c1f3ae2293b
fuziki <hymne.shannon.projects@gmail.com>
2022-08-13 21:07:04 +0900

    create Apps/skeleton

---
+++ Apps/skeleton/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  
+//  
+//  Created by fuziki on 2022/08/13
+//  
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
